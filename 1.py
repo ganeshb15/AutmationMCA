fdad547916661c9ef33cb2d0c997c715502fe503
ganeshb15 <61306528+ganeshb15@users.noreply.github.com>
2023-01-14 16:45:19 +0530

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys 
+from selenium.webdriver.support.ui import Select
+import time
